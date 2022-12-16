3766e64aee0744000dc6cbc97c044e44cc513b77
dimensioncodes <118059388+dimensioncodes@users.noreply.github.com>
2022-12-16 12:04:05 +0000

    update

--- SERVER/core.sh
+++ SERVER/core.sh
@@ -11,35 +11,43 @@ AppUser=$app_user
 AppPass=$app_pass
 
 
-
-cd SERVER
 mkdir $AppName
 cd $AppName
 cat <<EOF > docker-compose.yml
-$AppName:
-    image: wordpress
-    links:
-     - mariadb_$AppName:mysql
-    environment:
-     - WORDPRESS_DB_PASSWORD=$AppPass
-    ports:
-     - "$AppPort:80"
-    volumes:
-     - ./html_$AppName:/var/www/html
-    restart: always
+version: '3.1'
 
-mariadb_$AppName:
-    image: mariadb
+services:
+
+  wordpress:
+    image: wordpress
+    restart: always
+    ports:
+      - $AppPort
     environment:
-     - MYSQL_ROOT_PASSWORD=$AppPass
-     - MYSQL_DATABASE_0=wordpress_$AppName
+      WORDPRESS_DB_HOST: db$AppName
+      WORDPRESS_DB_USER: $AppUser
+      WORDPRESS_DB_PASSWORD: $AppPass
+      WORDPRESS_DB_NAME: db
     volumes:
-     - ./database_$AppName:/var/lib/mysql
-    restart: always 
+      - wordpress:/var/www/html
+
+  db:
+    image: mysql:5.7
+    restart: always
+    environment:
+      MYSQL_DATABASE: db$AppName
+      MYSQL_USER: $AppUser
+      MYSQL_PASSWORD: $AppPass
+      MYSQL_RANDOM_ROOT_PASSWORD: '1'
+    volumes:
+      - db:/var/lib/mysql
+
+volumes:
+  wordpress:
+  db:
 EOF
 echo DimensionCloud run "$AppName" --public --cloud --port $AppPort
 wait
 docker compose -p $AppName up -d
-cd ..
 cd ..
 done
